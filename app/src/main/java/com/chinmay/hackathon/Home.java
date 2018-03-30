c8fd643599463f4427771996a2c9046c74c43c2e
EngineeringManager <34608419+EngineeringManager@users.noreply.github.com>
2018-03-30 09:37:06 +0530

    Splash Activity Done
    Webview/Intent  not working

--- app/src/main/java/com/chinmay/hackathon/Home.java
+++ app/src/main/java/com/chinmay/hackathon/Home.java
@@ -1,10 +1,13 @@
 package com.chinmay.hackathon;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class Home extends AppCompatActivity {
@@ -16,6 +19,9 @@ public class Home extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +30,23 @@ public class Home extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
+
+    public void goGoogle( ) {
+        Log.i("Check ","In loop");
+        Intent intent = new Intent(this, webview.class);
+        intent.putExtra("url", "https://google.com");
+
+        startActivity(intent);
+    }
+
+    public void goFlipkart() {
+        Intent intent = new Intent(this, webview.class);
+        intent.putExtra("url", "https://flipkart.com");
+
+        this.startActivity(intent);
+    }
+
 
 }
---
+++ app/src/main/java/com/chinmay/hackathon/webview.java
@@ -0,0 +1,34 @@
+package com.chinmay.hackathon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class webview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        if (getIntent() != null) {
+            final WebView webview1;
+
+            String info = getIntent().getStringExtra("url");
+            webview1 = (WebView) findViewById(R.id.webview);
+            webview1.getSettings().setJavaScriptEnabled(true);
+            webview1.loadUrl(info);
+          //  finishAndRemoveTask();
+
+        }
+
+    }
+
+
+}
